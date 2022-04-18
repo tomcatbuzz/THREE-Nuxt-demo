671f228f7123c7e894368da12d660bb6c95f6cd1
Tony <tomcatbuzz@yahoo.com>
2022-04-17 21:45:22 -0400

    wip style and canvas2

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,7 +9,7 @@ import { MyWorkComponent } from '../../AngularWebV2/src/app/my-work/my-work.comp
         </NuxtLink>
       </h1>
       <nav>
-        <ul class="menu">
+        <ul>
           <li>
             <NuxtLink to="/">
               Home
@@ -25,10 +25,18 @@ import { MyWorkComponent } from '../../AngularWebV2/src/app/my-work/my-work.comp
               Contact
             </NuxtLink>
           </li>
-          <li>
+          <!-- <li>
             <MaterialIconMenu id="menu" title="menu" />
-          </li>
+          </li> -->
         </ul>
+        <button
+          class="js-menu menu button-clear menu-white menu-spinkit"
+          aria-label="Site navigation"
+        >
+          <span class="menu-icon-line-1 menu-icon-line" />
+          <span class="menu-icon-line-2 menu-icon-line" />
+          <span class="menu-icon-line-3 menu-icon-line" />
+        </button>
       </nav>
     </header>
     <main>
@@ -67,73 +75,168 @@ export default {
 }
 </script>
 <style>
-  * {
+* {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  font-family: 'Poppins';
+  font-family: "Poppins";
   -webkit-font-smoothing: antialiased;
   overflow: hidden;
-  }
+}
 
-  header {
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
-    height: 2.5em;
-    width: 100%;
-  }
+header {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  /* height: 2.5em; */
+  height: 3em;
+  width: 100%;
+  background-color: orange;
+}
 
-  #title {
-    margin-right: auto;
-    font-size: 1.5em;
-  }
+#title {
+  margin-right: auto;
+  font-size: 1.5em;
+}
 
-  #title a {
-    color: black;
-    text-decoration: none;
-    list-style: none;
-  }
+#title a {
+  color: black;
+  text-decoration: none;
+  list-style: none;
+}
 
-  header li {
-    display: inline-block;
-    justify-content: space-between;
-  }
+header li {
+  display: inline-block;
+  justify-content: space-between;
+}
 
-  li a {
-    list-style: none;
-    text-decoration: none;
-    color: black;
-    font-size: 1em;
-    text-transform: uppercase
-  }
+li a {
+  list-style: none;
+  text-decoration: none;
+  color: black;
+  font-size: 1em;
+  text-transform: uppercase;
+  margin-right: 2em;
+}
 
-  #menu {
-    color: blue;
-  }
+ul {
+  margin-right: 3em;
+}
 
-  .nuxt-link-exact-active:not(#title a) {
-    color: red;
-    font-weight: bold;
-    transform: scaleZ(2.5);
-  }
+.button-clear {
+  border: none;
+  background-color: transparent;
+}
 
-  .nuxt-link-exact-active:not(#title a) {
-    transition: transform 2s, font-weight 1.5s, color 1s;
-  }
+.button-clear:focus {
+  outline: 0;
+}
 
-  .container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100vw;
-    height: 100vh;
-    background-color: black;
-  }
+.menu {
+  transform: translateZ(100px);
+  top: 10px;
+  right: 10px;
+}
 
-  footer {
-    display: flex;
-    height: 2.5em;
-    width: 100%;
-  }
+.nav {
+  transform: translateZ(100px);
+}
+
+.menu-spinkit .menu-icon-line {
+  background-color: rgba(255, 255, 255, 0.7);
+}
+
+.menu-spinkit:hover > .menu-icon-line,
+.menu-spinkit.menu-active > .menu-icon-line {
+  background-color: #fff;
+}
+
+.menu {
+  position: absolute;
+  top: 0.5vmin;
+  right: 1.7vmin;
+  margin-left: 1em;
+  z-index: 2004;
+  outline: none;
+  -webkit-tap-highlight-color: transparent;
+  width: 43px;
+  padding: 10px;
+  cursor: pointer;
+}
+
+.menu-hidden {
+  visibility: hidden;
+  pointer-events: none;
+}
+
+.menu-icon-line {
+  display: block;
+  height: 2px;
+  width: 100%;
+  background-color: #a5acbc;
+  margin-bottom: 4px;
+  border-radius: 2px;
+  transition: transform 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275),
+    opacity 0.2s ease-out;
+  transform: translateZ(0);
+}
+
+.menu:hover > .menu-icon-line {
+  background-color: #8591ac;
+  transform: scaleX(1.2);
+}
+
+.menu-active > .menu-icon-line-1,
+.menu-active:hover > .menu-icon-line-1 {
+  transform: translateY(6px) rotateZ(-135deg);
+  background-color: #fff;
+}
+
+.menu-active > .menu-icon-line-2,
+.menu-active:hover > .menu-icon-line-2 {
+  opacity: 0;
+  background-color: #fff;
+}
+
+.menu-active > .menu-icon-line-3,
+.menu-active:hover > .menu-icon-line-3 {
+  transform: translateY(-6px) rotateZ(135deg);
+  background-color: #fff;
+}
+
+/* .nuxt-link-exact-active:not(#title a) {
+    visibility: hidden;
+  } */
+
+.nuxt-link-exact-active:not(#title a) {
+  color: red;
+  font-weight: bold;
+}
+
+.nuxt-link-exact-active:not(#title a) {
+  transition: font-weight 1.5s, color 1s;
+}
+
+a:hover {
+  color: green;
+  transform: scaleZ(2)
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: black;
+}
+
+footer {
+  position: fixed;
+  bottom: 0;
+  display: flex;
+  background-color: white;
+  height: 2.5em;
+  width: 100%;
+}
 </style>
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,14 +1,174 @@
 <template>
-  <div class="wrapper">
-    <h1>
-      contact
-    </h1>
+  <div>
+    <canvas ref="canvas2" />
+    <div class="wrapper">
+      <h1>
+        contact
+      </h1>
+    </div>
   </div>
 </template>
 
 <script>
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+// eslint-disable-next-line import/no-named-as-default
+import gsap from 'gsap'
 export default {
-  name: 'ContactPage'
+  name: 'ContactPage',
+  mounted () {
+    const raycaster = new THREE.Raycaster()
+    const scene = new THREE.Scene()
+    const camera = new THREE.PerspectiveCamera(
+      70,
+      innerWidth / innerHeight,
+      0.1,
+      1000)
+    const renderer = new THREE.WebGLRenderer({
+      canvas: this.$refs.canvas2,
+      antialias: true
+    })
+
+    camera.position.z = 50
+
+    renderer.setSize(innerWidth, innerHeight)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    // document.body.appendChild(renderer.domElement)
+
+    // eslint-disable-next-line no-new
+    new OrbitControls(camera, renderer.domElement)
+
+    const backlight = new THREE.DirectionalLight(0xFFFFFF, 1)
+    backlight.position.set(0, 1, -1)
+    scene.add(backlight)
+
+    const light = new THREE.DirectionalLight(0xFFFFFF, 1)
+    light.position.set(0, -1, 1)
+    scene.add(light)
+
+    const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
+    const material = new THREE.MeshBasicMaterial({ color: 0x00FF00 })
+    const mesh = new THREE.Mesh(boxGeometry, material)
+    scene.add(mesh)
+
+    const mouse = {
+      x: undefined,
+      y: undefined
+    }
+
+    // let frame = 0
+    function animate () {
+      requestAnimationFrame(animate)
+      renderer.render(scene, camera)
+      // frame += 0.01
+      // raycaster for point hover
+      raycaster.setFromCamera(mouse, camera)
+      const intersects = raycaster.intersectObject(mesh)
+      if (intersects.length > 0) {
+        const { color } = intersects[0].object.geometry.attributes
+
+        intersects[0].object.geometry.attributes.color.needsUpdate = true
+
+        const initialColor = {
+          r: 0,
+          g: 0.19,
+          b: 0.4
+        }
+
+        const hoverColor = {
+          r: 0.1,
+          g: 0.5,
+          b: 1
+        }
+
+        gsap.to(hoverColor, {
+          r: initialColor.r,
+          g: initialColor.g,
+          b: initialColor.b,
+          duration: 1,
+          onUpdate: () => {
+            // vertice 1
+            color.setX(intersects[0].face.a, hoverColor.r)
+            color.setY(intersects[0].face.a, hoverColor.g)
+            color.setZ(intersects[0].face.a, hoverColor.b)
+
+            // vertice 2
+            color.setX(intersects[0].face.b, hoverColor.r)
+            color.setY(intersects[0].face.b, hoverColor.g)
+            color.setZ(intersects[0].face.b, hoverColor.b)
+
+            // vertice 3
+            color.setX(intersects[0].face.c, hoverColor.r)
+            color.setY(intersects[0].face.c, hoverColor.g)
+            color.setZ(intersects[0].face.c, hoverColor.b)
+          }
+        })
+      }
+      mesh.rotateX(0.005)
+    }
+
+    animate()
+
+    addEventListener('mousemove', (e) => {
+      mouse.x = (e.clientX / innerWidth) * 2 - 1
+      mouse.y = -(e.clientY / innerHeight) * 2 + 1
+      // console.log(mouse)
+    })
+
+    // gsap.to('#name', {
+    //   opacity: 1,
+    //   duration: 1.5,
+    //   y: 0,
+    //   ease: 'power2.out'
+    // })
+    // gsap.to('#headline', {
+    //   opacity: 1,
+    //   duration: 1.5,
+    //   delay: 0.3,
+    //   y: 0,
+    //   ease: 'power2.out'
+    // })
+    // gsap.to('#button', {
+    //   opacity: 1,
+    //   duration: 1.5,
+    //   delay: 0.6,
+    //   y: 0,
+    //   ease: 'power2.out'
+    // })
+
+    // document.querySelector('#button')
+    //   .addEventListener('click', (e) => {
+    //     e.preventDefault()
+    //     gsap.to('#app', {
+    //       opacity: 0
+    //     })
+    //     gsap.to(camera.position, {
+    //       z: 25,
+    //       ease: 'power1.inOut',
+    //       duration: 2
+    //     })
+    //     gsap.to(camera.rotation, {
+    //       x: 1.57,
+    //       ease: 'power1.inOut',
+    //       duration: 2
+    //     })
+    //     gsap.to(camera.position, {
+    //       y: 1000,
+    //       ease: 'power1.in',
+    //       duration: 1,
+    //       delay: 1.5,
+    //       onComplete: () => {
+    //         this.$router.push('/work')
+    //       }
+    //     })
+    //   })
+
+    addEventListener('resize', () => {
+      camera.aspect = innerWidth / innerHeight
+      camera.updateProjectionMatrix()
+      renderer.setSize(innerWidth, innerHeight)
+    })
+  }
 }
 </script>
 
