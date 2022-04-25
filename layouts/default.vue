47781f714fc1431fa8a50c78218aa2e0973ab982
Tony <tomcatbuzz@yahoo.com>
2022-04-25 14:36:37 -0400

    wip nav, need to undo

--- layouts/default.vue
+++ layouts/default.vue
@@ -25,7 +25,7 @@
             </NuxtLink>
           </li>
           <li>
-            <button>
+            <button id="menu">
               Bitch Button
             </button>
           </li>
@@ -33,6 +33,86 @@
       </nav>
       <!-- <FullNav /> -->
     </header>
+    <!-- <div class="overlay">
+      <div class="close">
+        <button class="close">
+          Close
+        </button>
+      </div>
+      <div class="menu-content">
+        <ul>
+          <li>Home</li>
+          <li>Work</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div> -->
+    <div class="full-nav">
+      <!-- <button class="menu-btn">
+        <div class="content">
+          <span class="text">show</span>
+          <span class="text">hide</span>
+        </div>
+      </button> -->
+      <nav class="nav">
+        <div class="nav__inner">
+          <div class="nav--transition-slide" />
+          <div class="closeBtn">
+            <button class="close">
+              Close
+            </button>
+          </div>
+          <div class="nav--items">
+            <div class="nav--item">
+              <div class="nav--item-link">
+                <a href="#" class="nav--link">
+                  <span class="nav--link-text" data-text="projects">projects</span>
+                  <div class="nav--link-icon">
+                    <svg class="icon">
+                      <use xlink:href="#arrow-right" />
+                    </svg>
+                  </div>
+                </a>
+              </div>
+              <div class="nav--item-line" />
+            </div>
+            <div class="nav--item">
+              <div class="nav--item-link">
+                <a href="#" class="nav--link">
+                  <span class="nav--link-text" data-text="about">about</span>
+                  <div class="nav--link-icon">
+                    <svg class="icon">
+                      <use xlink:href="#arrow-right" />
+                    </svg>
+                  </div>
+                </a>
+              </div>
+              <div class="nav--item-line" />
+            </div>
+            <div class="nav--item">
+              <div class="nav--item-link">
+                <a href="#" class="nav--link">
+                  <span class="nav--link-text" data-text="contact">contact</span>
+                  <div class="nav--link-icon">
+                    <svg class="icon">
+                      <use xlink:href="#arrow-right" />
+                    </svg>
+                  </div>
+                </a>
+              </div>
+              <div class="nav--item-line" />
+            </div>
+          </div>
+        </div>
+      </nav>
+      <svg style="display: none">
+        <symbol id="arrow-right" viewBox="0 0 492.004 492.004">
+          <path
+            d="M484.14,226.886L306.46,49.202c-5.072-5.072-11.832-7.856-19.04-7.856c-7.216,0-13.972,2.788-19.044,7.856l-16.132,16.136c-5.068,5.064-7.86,11.828-7.86,19.04c0,7.208,2.792,14.2,7.86,19.264L355.9,207.526H26.58C11.732,207.526,0,219.15,0,234.002v22.812c0,14.852,11.732,27.648,26.58,27.648h330.496L252.248,388.926c-5.068,5.072-7.86,11.652-7.86,18.864c0,7.204,2.792,13.88,7.86,18.948l16.132,16.084c5.072,5.072,11.828,7.836,19.044,7.836c7.208,0,13.968-2.8,19.04-7.872l177.68-177.68c5.084-5.088,7.88-11.88,7.86-19.1C492.02,238.762,489.228,231.966,484.14,226.886z"
+          />
+        </symbol>
+      </svg>
+    </div>
     <main>
       <div class="container">
         <Nuxt />
@@ -46,7 +126,7 @@
 
 <script>
 // eslint-disable-next-line import/no-named-as-default
-// import gsap from 'gsap'
+import gsap from 'gsap'
 // import FullNav from '@/components/FullNav'
 export default {
   name: 'DefaultLayout',
@@ -72,7 +152,113 @@ export default {
       title: 'Tomcatbuzz'
     }
   },
-  mounted () {}
+  mounted () {
+    // document.querySelector('#menu')
+    //   .addEventListener('click', () => {
+    //     // e.preventDefault()
+    //     gsap.fromTo('.overlay', {
+    //       scaleX: 0,
+    //       transformOrigin: 'left center'
+    //     },
+    //     {
+    //       duration: 0.6,
+    //       scaleX: 1,
+    //       // ease: 'power1.inOut'
+    //       ease: 'none'
+    //     })
+    //   })
+
+    // document.querySelector('.close')
+    //   .addEventListener('click', () => {
+    //     // e.preventDefault()
+    //     gsap.to('.overlay', {
+    //       scaleX: 1,
+    //       transformOrigin: 'right center'
+    //       duration: 0.6,
+    //       // ease: 'power1.inOut'
+    //       ease: 'none'
+    //     }
+
+    // try this here
+    document.querySelector('#menu')
+      .addEventListener('click', () => {
+        show()
+      })
+    function show () {
+      const tl = gsap.timeline()
+
+      gsap.set('.nav__inner', {
+        pointerEvents: 'none'
+      })
+
+      tl.fromTo('.nav--transition-slide', {
+        scaleX: 0,
+        transformOrigin: 'left center'
+      },
+      {
+        duration: 0.5,
+        scaleX: 1,
+        ease: 'Expo.inOut'
+      }
+      )
+        .set('.nav__inner', {
+          pointerEvents: 'all'
+        })
+        .fromTo('.nav--item-line', {
+          scaleX: 0,
+          transformOrigin: 'left center'
+        },
+        {
+          duration: 0.65,
+          scaleX: 1,
+          ease: 'Expo.inOut',
+          stagger: 0.15
+        }
+        )
+        .fromTo('.nav--link', {
+          translateY: '100%'
+        },
+        {
+          duration: 2.25,
+          translateY: 0,
+          ease: 'elastic.inOut',
+          stagger: 0.15
+        }, '-=1.65')
+    }
+
+    document.querySelector('.close')
+      .addEventListener('click', () => {
+        hide()
+      })
+
+    function hide () {
+      const tl = gsap.timeline()
+
+      gsap.set('.nav__inner', {
+        pointerEvents: 'none'
+      })
+
+      tl.to('.nav--item-line', {
+        duration: 0.6,
+        scaleX: 0,
+        transformOrigin: 'right center',
+        ease: 'Expo.inOut',
+        stagger: -0.15
+      })
+        .to('.nav--link', {
+          duration: 0.35,
+          translateY: '100%',
+          ease: 'Expo.inOut',
+          stagger: -0.15
+        }, 0)
+        .to('.nav--transition-slide', {
+          duration: 0.5,
+          transformOrigin: 'right center',
+          scaleX: 0,
+          ease: 'Expo.inOut'
+        })
+    }
+  }
 }
 </script>
 <style>
@@ -134,8 +320,9 @@ ul {
   } */
 
 .nuxt-link-exact-active:not(#title a) {
-  color: red;
+  background-color: red;
   font-weight: bold;
+  font-size: 1.2em;
 }
 
 /* li a:hover {
@@ -220,5 +407,142 @@ footer {
 
 .page-leave-to {
   opacity: 0;
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: black;
+  transform: scaleX(0);
+  z-index: 100;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.menu-content ul li {
+  display: flex;
+  flex-direction: column;
+  color: white
+}
+
+.close {
+  position: fixed;
+  top: 2em;
+  right: 2em;
+  padding: 0.5em;
+  font-family: 'Montserrat';
+  text-transform: uppercase;
+  background-color: transparent;
+  color: rgb(157, 151, 151);
+  font-size: 1em;
+}
+
+@media screen and (max-width: 480px) and (orientation: portrait) {
+  li:not(:nth-child(4)) {
+    display: none;
+  }
+}
+
+.nav {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100vh;
+  z-index: 5;
+}
+.nav .nav__inner {
+  width: 100%;
+  height: 100%;
+  pointer-events: none;
+}
+.nav .nav__inner .nav--items {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.nav .nav__inner .nav--items .nav--item {
+  position: relative;
+  width: 50%;
+  height: 120px;
+  margin-bottom: 2rem;
+  overflow: hidden;
+}
+.nav .nav__inner .nav--items .nav--item-link .nav--link {
+  display: block;
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+  transform: translateY(100%);
+}
+.nav .nav__inner .nav--items .nav--item-link .nav--link-text {
+  height: 100%;
+  position: relative;
+  /* font-family: "Kanit"; */
+  font-family: "Montserrat";
+  font-weight: 800;
+  font-size: 85px;
+  text-transform: uppercase;
+  letter-spacing: 6px;
+  color: transparent;
+  -webkit-text-stroke: 2px rgb(235, 235, 235);
+}
+.nav .nav__inner .nav--items .nav--item-link .nav--link-text::after {
+  content: attr(data-text);
+  position: absolute;
+  left: 0;
+  top: 0;
+  color: rgb(235, 235, 235);
+  clip-path: polygon(0 100%, 100% 100%, 100% 100%, 0 100%);
+  transition: clip-path 400ms ease;
+}
+.nav .nav__inner .nav--items .nav--item-link .nav--link-icon {
+  transform: translateX(-35%);
+  opacity: 0;
+  transition: transform 250ms ease, opacity 100ms linear;
+  margin-left: 25px;
+}
+.nav .nav__inner .nav--items .nav--item-link .nav--link-icon svg {
+  width: 100px;
+  height: 100px;
+  fill: rgb(235, 235, 235);
+}
+.nav
+  .nav__inner
+  .nav--items
+  .nav--item-link
+  .nav--link:hover
+  .nav--link-text::after {
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
+}
+.nav .nav__inner .nav--items .nav--item-link .nav--link:hover .nav--link-icon {
+  transform: translateX(0%);
+  opacity: 1;
+}
+
+.nav .nav__inner .nav--items .nav--item-line {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  height: 2px;
+  width: 100%;
+  background: rgb(235, 235, 235);
+  transform: scaleX(0);
+}
+.nav .nav__inner .nav--transition-slide {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background: rgb(19, 19, 19);
+  transform: scaleX(0);
 }
 </style>
